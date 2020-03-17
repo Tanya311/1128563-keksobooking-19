35c2cd24694e04b128b440258b839023266e0ba8
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-03-17 10:52:35 +0300

    fix map

--- js/map.js
+++ js/map.js
@@ -11,54 +11,94 @@
   var formResetButton = document.querySelector('.ad-form__reset');
 
   /**
-   * функция активации и деактивации страницы
+   * @name activateForm
+   * @description функция разблокировки формы
+   */
+  var activateForm = function () {
+    adForm.classList.remove('ad-form--disabled');
+    adFormFieldsets.forEach(function (formFieldset) {
+      formFieldset.removeAttribute('disabled');
+    });
+    adressAdForm.value = (window.util.pinDate.X_START + Math.round(window.util.pinDate.WIDTH / 2)) + ' , ' + (window.util.pinDate.Y_START + window.util.pinDate.HEIGHT);
+    adForm.addEventListener('submit', window.form.submitHandler);
+    adForm.addEventListener('change', window.form.changeHandler);
+  };
+
+  /**
+   * @name disabledForm
+   * @description функция блокировки формы
+   */
+  var disabledForm = function () {
+    adForm.classList.add('ad-form--disabled');
+    adForm.reset();
+    adressAdForm.value = Math.round(window.util.pinDate.X_START + window.util.pinDate.WIDTH / 2) + ' , ' + Math.round(window.util.pinDate.Y_START + window.util.pinDate.HIGHT_PIN / 2);
+    adressAdForm.setAttribute('readonly', 'readonly');
+    adFormFieldsets.forEach(function (fieldset) {
+      fieldset.setAttribute('disabled', 'disabled');
+    });
+  };
+
+  /**
+   * @name activateFilter
+   * @description функция разблокировки фильтров
+   */
+  var activateFilter = function () {
+    mapFiltersFormSelect.forEach(function (filterSelect) {
+      filterSelect.removeAttribute('disabled');
+    });
+    mapFiltersFormFieldsets.forEach(function (filterFieldset) {
+      filterFieldset.removeAttribute('disabled');
+    });
+  };
+
+  /**
+   * @name disabledFilter
+   * @description функция блокировки фильтров
+   */
+  var disabledFilter = function () {
+    mapFiltersFormSelect.forEach(function (filterSelect) {
+      filterSelect.removeAttribute('disabled');
+    });
+    mapFiltersFormFieldsets.forEach(function (filterFieldset) {
+      filterFieldset.removeAttribute('disabled');
+    });
+  };
+
+  /**
+   * @name activatePage
+   * @description функция активации и деактивации страницы
    * @param {boolean} active
    */
   var activatePage = function (active) {
     if (active) {
       mapDialog.classList.remove('map--faded');
       window.backend.load(successHandler, window.backend.errorHandler);
-      adForm.classList.remove('ad-form--disabled');
-      adFormFieldsets.forEach(function (formFieldset) {
-        formFieldset.removeAttribute('disabled');
-      });
-      mapFiltersFormSelect.forEach(function (filterSelect) {
-        filterSelect.removeAttribute('disabled');
-      });
-      mapFiltersFormFieldsets.forEach(function (filterFieldset) {
-        filterFieldset.removeAttribute('disabled');
-      });
-      adressAdForm.value = (window.util.pinDate.X_START + Math.round(window.util.pinDate.WIDTH / 2)) + ' , ' + (window.util.pinDate.Y_START + window.util.pinDate.HEIGHT);
+      activateForm();
       mapPinsButton.removeEventListener('mousedown', pageActiveHandler);
       mapPinsButton.removeEventListener('keydown', pageActiveHandler);
-      adForm.addEventListener('submit', window.form.submitHandler);
-      adForm.addEventListener('change', window.form.changeHandler);
+      formResetButton.addEventListener('click', formResetButtonHandler);
+
     } else {
       mapDialog.classList.add('map--faded');
       window.pin.remove();
       window.card.remove();
-      adForm.classList.add('ad-form--disabled');
-      adForm.reset();
+
       mapPinsButton.style.top = window.util.pinDate.Y_START + 'px';
       mapPinsButton.style.left = window.util.pinDate.X_START + 'px';
-      adressAdForm.value = Math.round(window.util.pinDate.X_START + window.util.pinDate.WIDTH / 2) + ' , ' + Math.round(window.util.pinDate.Y_START + window.util.pinDate.HIGHT_PIN / 2);
-      adressAdForm.setAttribute('readonly', 'readonly');
-      mapFiltersFormSelect.forEach(function (select) {
-        select.setAttribute('disabled', 'disabled');
-        select.value = 'any';
-      });
-      mapFiltersFormFieldsets.forEach(function (fieldset) {
-        fieldset.setAttribute('disabled', 'disabled');
-      });
-      adFormFieldsets.forEach(function (fieldset) {
-        fieldset.setAttribute('disabled', 'disabled');
-      });
+
+      disabledFilter();
+      disabledForm();
+
       mapPinsButton.addEventListener('mousedown', pageActiveHandler);
       mapPinsButton.addEventListener('keydown', pageActiveHandler);
     }
   };
 
-
+  /**
+   * @name pageActiveHandler
+   * @description функция активации страницы по нажатию на кнопку мыши
+   * @param {evt} evt
+   */
   var pageActiveHandler = function (evt) {
     if (evt.button === window.util.mousedownLeftButton) {
       activatePage(true);
@@ -67,19 +107,28 @@
 
   activatePage(false);
 
-  formResetButton.addEventListener('click', function () {
+  /**
+   * @name formResetButtonHandler
+   * @description функция дезактивации страницы по нажатию на кнопку reset
+   */
+  var formResetButtonHandler = function () {
     activatePage(false);
-  });
+    formResetButton.removeEventListener('click', formResetButtonHandler);
+  };
 
+  /**
+   * @name successHandler
+   * @description функция отрабатывается при загрузке данных
+   * @param {*} data
+   */
   var successHandler = function (data) {
     window.defaultAdverts = data;
     window.pin.render(data);
+    activateFilter();
   };
-
 
   window.map = {
     activatePage: activatePage,
-    pageActiveHandler: pageActiveHandler,
   };
 
 })();
